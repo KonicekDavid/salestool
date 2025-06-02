470c075371eb8003f95dbefeeaab1af2065563de
KonicekDavid <konicekdavid323@gmail.com>
2025-06-02 13:33:21 +0200

    Fix error presenters

--- app/Presentation/Error/Error4xxPresenter.php
+++ app/Presentation/Error/Error4xxPresenter.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
-namespace App\Presentation\Error\Error4xx;
+namespace App\Presentation\Error;
 
 use Nette;
 use Nette\Application\Attributes\Requires;
+use Nette\Application\Responses\JsonResponse;
 
 
 /**
@@ -16,6 +17,8 @@ final class Error4xxPresenter extends Nette\Application\UI\Presenter
 {
 	public function renderDefault(Nette\Application\BadRequestException $exception): void
 	{
+        $response = new JsonResponse(['message' => 'Bad request.']);
 		$this->getHttpResponse()->setCode($exception->getCode());
+        $this->sendResponse($response);
 	}
 }
